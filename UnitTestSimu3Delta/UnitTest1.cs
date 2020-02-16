78fb9f807781d3dfee47e4f0979f47723f98826e
Emile <hugues@pc-emile>
2020-02-16 20:53:03 +0100

    initial commit

---
+++ UnitTestSimu3Delta/UnitTest1.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestSimu3Delta
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(true, false);
+        }
+        [TestMethod]
+        public void TestRacineCarreDe0()
+        {
+            Assert.AreEqual(true, true);
+        }
+    }
+}
